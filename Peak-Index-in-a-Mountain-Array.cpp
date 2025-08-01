9db1c99110a51d8f0c04d38d1a85b5931ee31d56
Ravi-gurugram <125189351+Ravi-gurugram@users.noreply.github.com>
2025-08-01 18:48:50 +0530

    [852] [Time Beats: 100.00%] [Memory Beats: 47.85%] - LeetPush

---
+++ Peak-Index-in-a-Mountain-Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int start=0;
+        int end=arr.size()-1;
+        if(end==0)
+         return arr[0] ; int mid;
+        while(start<=end){
+           
+           mid= end+ (start-end)/2 ;
+            if(arr[mid]>arr[mid-1]&&arr[mid]>arr[mid+1])
+              break;
+            else if(arr[mid]>arr[mid-1])
+               start= mid+1 ;
+            else
+              end = mid-1;
+        }
+         return mid ;
+    }
+};
